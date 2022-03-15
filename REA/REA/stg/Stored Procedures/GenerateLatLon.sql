32b864053d366c379ecb7329fe9a96b0528ae412
Tim <tim@jawslife.us>
2022-03-14 18:36:07 -0700

    Update Proce [stg].[GenerateLatLon]

--- REA/REA/stg/Stored Procedures/GenerateLatLon.sql
+++ REA/REA/stg/Stored Procedures/GenerateLatLon.sql
@@ -1,21 +1,29 @@
-﻿
-create   proc stg.GenerateLatLon
+﻿USE [REA]
+GO
+/****** Object:  StoredProcedure [stg].[GenerateLatLon]    Script Date: 3/14/2022 6:12:10 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+Create   proc [stg].[GenerateLatLon]
 
 
 @latStart decimal(9,6),
 @latEnd decimal(9,6),
 @lonStart decimal(9,6),
 @lonEnd decimal(9,6),
-@size decimal(9,6)
+@size decimal(9,6),
+@lev int
 
 as
 
 
---Exec  stg.GenerateLatLon 32, -126, 42.2, -113, 0.5
+--Exec [stg].[GenerateLatLon] 32, 42.2, -126, -113, 0.04, 2
 ----Create above Stored Procedure, populate to table stg.Locations with all location values
 
 
-truncate table stg.Locations
+--truncate table stg.Locations
 
 --Declare @latStart decimal(9,6) = 32
 --Declare @latEnd decimal(9,6) = 42.2
@@ -40,7 +48,7 @@ BEGIN
 		set @LatB = @LatA + @size
 	
 		Print 'Range is: from (' + cast(@LatA as varchar(255)) + ', ' + cast(@LonA as varchar(255)) + ') to (' + cast(@LatA as varchar(255)) + ', ' + cast(@LonA as varchar(255)) + ')'
-		Insert into stg.Locations (RowId, ColId, LatA, LonA, LatB, LonB) Values (@RowId, @ColId, @LatA, @LonA, @LatB, @LonB)
+		Insert into stg.Locations (RowId, ColId, LatA, LonA, LatB, LonB, Lev) Values (@RowId, @ColId, @LatA, @LonA, @LatB, @LonB, @lev)
 	
 		set @LatA = @LatA + @size
 		set @ColId = @ColId + 1
@@ -49,3 +57,8 @@ BEGIN
 	set @RowId = @RowId + 1
 	Print '----------------------------------------------------------'
 END
+
+
+
+
+
